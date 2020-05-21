a2b4b7fb404f58ceb1e473383717cc351ee3a3ac
Shreyas Ubale <shreyas.ubale@exponential.com>
2020-05-21 14:33:32 +0530

    committing demo

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,8 @@ import Header from './Header.js';
 import VideoList from './VideoList.js';
 import Video from './Video';
 import './App.css';
+import canAutoplay from "can-autoplay";
+
 
 /* 
  * Main component.
@@ -12,15 +14,85 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.autoplayAllowed = false;
+    this.autoplayRequiresMute = true;
     this.state = {
       ui: {
         header: 'My Video SPA',
         headerVideoList: 'Top 10s',
       },
-      data: []
+      key: 0,
+      data: [],
+      opts: {
+        autoPlay: true,
+        playsinline : true,
+        inline: true,
+        controls: true,
+        muted: true,
+        sources: [{
+          src: "http://techslides.com/demos/sample-videos/small.mp4"
+        }]
+      }
     };
     this.activeVideo = this.activeVideo.bind(this);
   }
+
+  checkUnmutedAutoplaySupport() {
+    canAutoplay
+      .video({
+        timeout: 500,
+        muted: false,
+        inline: true
+      })
+      .then(function(response) {
+        if (response.result === false) {
+          // Unmuted autoplay is not allowed.
+          this.checkMutedAutoplaySupport();
+        } else {
+          // Unmuted autoplay is allowed.
+          this.autoplayAllowed = true;
+          this.autoplayRequiresMute = false;
+          this.fetchData();
+        }
+      }.bind(this))
+  };
+  fetchData() {
+    fetch('./data.json').then(response => {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ', response.status);
+        return;
+      }
+      response.json().then(data => {
+        this.setState({
+          data: data.videos
+        });
+      });
+    }).catch(error => {
+      console.log('There has been a problem with your fetch operation: ', error.message);
+    });
+  }
+
+  checkMutedAutoplaySupport() {
+    canAutoplay
+      .video({
+        timeout: 500,
+        muted: true,
+        inline: true
+      })
+      .then(function(response) {
+        if (response.result === false) {
+          // Muted autoplay is not allowed.
+          this.autoplayAllowed = false;
+          this.autoplayRequiresMute = false;
+        } else {
+          // Muted autoplay is allowed.
+          this.autoplayAllowed = true;
+          this.autoplayRequiresMute = true;
+        }
+        this.fetchData();
+      }.bind(this))
+  }
+
   activeVideo(id) {
     // activate a video
     let data = [...this.state.data];
@@ -31,40 +103,34 @@ class App extends Component {
     let index = data.findIndex(video => video.id === id);
     data[index].isActive = true;
     this.setState({ data });
+    this.setState({key: this.state.key +1})
+    this.setState(
+        {
+          opts: Object.assign({},this.state.opts,{muted: false})
+        }
+    )
+    
+
+
   }
   componentDidMount() {
+
+    this.checkUnmutedAutoplaySupport()
+
     // fetch data from json and populate components
-    fetch('./data.json').then(response => {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ', response.status);
-        return;
-      }
-      response.json().then(data => {
-        this.setState({ data: data.videos });
-      });
-    }).catch(error => {
-      console.log('There has been a problem with your fetch operation: ', error.message);
-    });
   }
   render() {
     // find out active video
     let data = this.state.data;
     let activeVideo = data.find(video => video.isActive);
     console.log(activeVideo);
-    const opts = {
-        autoPlay: true,
-        controls: true,
-        muted: true,
-        sources: [{
-            src:(activeVideo && activeVideo.src) || "http://techslides.com/demos/sample-videos/small.mp4"
-        }]
-    };
+    const opts = this.state.opts;
     return (
       <div className="container">
         <div className='app row'>
           <div className='app__video row'>
               <div className='app__videoList col-md-6'>
-                  <Video {...opts}/>
+                  <Video key = {this.state.key} {...this.state.opts}/>
               </div>
 
               <div className='app__videoList col-md-4'>
--- app/src/Video.js
+++ app/src/Video.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Loader from './Loader.js';
 import ReactDOM from "react-dom";
 import './Video.css';
 import videojs from 'video.js';
@@ -17,21 +16,51 @@ import google from './Google';
 class Video extends Component {
     constructor(props) {
         super(props);
+        this.autoplayAllowed = false;
+        this.autoplayRequiresMute = false;
     }
 
-    componentDidMount() {
-        this.player = videojs(this.videoNode, this.props, () => {
+
+    initPlayer() {
+        // this.props.muted = this.autoplayRequiresMute;
+        this.player = videojs(this.videoNode,this.props, () => {
             console.log('onPlayerReady', this)
             // on ready function for anything else you need to do
             // after the player is set up…
-            var imaOptions = {
-                adTagUrl: 'http://a.beta.tribalfusion.com/v.vast?vpaid=1&clickTrackURL=//www.beta.tribalfusion.com/adapp/tf/campaign/click/3060944X4000375X437/500016311&mockApp=1&mediaType=24&mediaDataID=500016311&clientID=293233&env=instream&size=1x10&url=//www.beta.tribalfusion.com/adapp/tf/campaign/play/3060944X4000375X437/500016311&fileName=preroll.vpaid.xml',
-                vpaidMode : 2,
-                // autoPlayAdBreaks: false,
-                debug: true
-            };
-            this.player.ima(imaOptions);
+
+            // this.player.ima.playAdBreak();
         });
+        // this.player.muted(this.props.muted);
+        if(this.props.muted === false) {
+            this.player.volume(1);
+        }
+        var imaOptions = {
+            adTagUrl: 'http://a.beta.tribalfusion.com/v.vast?vpaid=1&clickTrackURL=//www.beta.tribalfusion.com/adapp/tf/campaign/click/3060944X4000375X437/500016311&mockApp=1&mediaType=24&mediaDataID=500016311&clientID=293233&env=instream&size=1x10&url=//www.beta.tribalfusion.com/adapp/tf/campaign/play/3060944X4000375X437/500016311&fileName=preroll.vpaid.xml',
+            vpaidMode: 2,
+            autoPlayAdBreaks: true,
+            debug: true
+        };
+        this.player.ima(imaOptions);
+        this.player.on("adsready", function() {
+            console.log("adssssss are ready");
+            // this.player.ima.playAdBreak();
+            this.player.play();
+        }.bind(this));
+        this.player.ima.initializeAdDisplayContainer();
+    }
+
+    componentDidMount() {
+        console.log("mounting component");
+        this.initPlayer();
+            
+            // this.player.ima.addEventListener(google.ima.AdEvent.Type.LOADED, function() {
+            //     console.log("ad break ready");
+            //     // if (visibleStatus > 50) {
+            //         this.player.ima.playAdBreak();
+            //     // }
+            //     // adReady = true;
+            // }.bind(this))
+            // this.player.ima.requestAds();
 
 
 
@@ -44,15 +73,26 @@ class Video extends Component {
     componentDidUpdate () {
         // let video = ReactDOM.findDOMNode(this);
         try {
-            this.player.ima.changeAdTag(null);
-            this.player && this.player.ima.changeAdTag('http://a.beta.tribalfusion.com/v.vast?vpaid=1&clickTrackURL=//www.beta.tribalfusion.com/adapp/tf/campaign/click/3060944X4000375X437/500016311&mockApp=1&mediaType=24&mediaDataID=500016311&clientID=293233&env=instream&size=1x10&url=//www.beta.tribalfusion.com/adapp/tf/campaign/play/3060944X4000375X437/500016311&fileName=preroll.vpaid.xml');
+            console.log("componentdidupdate");
+            this.player.ima.changeAdTag('http://a.beta.tribalfusion.com/v.vast?vpaid=1&clickTrackURL=//www.beta.tribalfusion.com/adapp/tf/campaign/click/3060944X4000375X437/500016311&mockApp=1&mediaType=24&mediaDataID=500016311&clientID=293233&env=instream&size=1x10&url=//www.beta.tribalfusion.com/adapp/tf/campaign/play/3060944X4000375X437/500016311&fileName=preroll.vpaid.xml')
+            this.player.ima.requestAds();
+            // this.adsManager && this.adsManager.destroy();
+            // this.player.ima.initializeAdDisplayContainer();
+            // this.player.ima.changeAdTag(null);
+            // this.player && this.player.ima.changeAdTag('http://a.beta.tribalfusion.com/v.vast?vpaid=1&clickTrackURL=//www.beta.tribalfusion.com/adapp/tf/campaign/click/3060944X4000375X437/500016311&mockApp=1&mediaType=24&mediaDataID=500016311&clientID=293233&env=instream&size=1x10&url=//www.beta.tribalfusion.com/adapp/tf/campaign/play/3060944X4000375X437/500016311&fileName=preroll.vpaid.xml');
         }catch(e) {
             console.log(e);
         }
-        this.player.ima.requestAds();
-        this.player.play();
+        
+        // this.player.on("adsready", function() {
+        //     console.log("adssssss are ready");
+        //     // this.player.play();
+        // }.bind(this));
+        
+        
     }
     componentWillUnmount() {
+        console.log("unmounting component");
         this.player.dispose();
        // whatever other things you need to clean up—maybe remove the DOM reference
         // this.videoPlayer = undefined;
@@ -60,7 +100,7 @@ class Video extends Component {
 
     render() {
         const vClassNames = classNames("video-js","vjs-16-9", "col-md-6");
-        console.log("Props are");
+      
 
         console.log(this.props);
         return (
